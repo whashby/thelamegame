c716069ffd39c783cc33f53b2157adb93f5b8982
Wafiq Harris-Ashby <whashby@gmail.com>
2025-04-23 09:22:20 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -850,31 +850,4 @@ document.addEventListener("DOMContentLoaded", () => {
         startGame(gameSetting);
     });
 
-
-
-
-
-
-
-
-
-
-    
-    const fs = require("fs");
-
-    const writeToFile = (filePath, data) => {
-        fs.writeFile(filePath, data, (err) => {
-            if (err) {
-                console.error("Error writing to file:", err);
-            } else {
-                console.log("File written successfully!");
-            }
-        });
-    };
-
-    // Example: Writing to "example.txt"
-    const data = "This is the data to be written to the file.";
-    writeToFile("example.txt", data);
-
-    
 });
