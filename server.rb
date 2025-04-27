780b13ac4a5ec5702e9be7d1839ac3cef0abd61e
whashby <whashby@gmail.com>
2025-04-27 16:09:33 -0400

    Update server.rb

--- server.rb
+++ server.rb
@@ -8,3 +8,18 @@ CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
 get '/' do
   erb :index, :locals => {:client_id => CLIENT_ID}
 end
+
+get '/callback' do
+  # get temporary GitHub code...
+  session_code = request.env['rack.request.query_hash']['code']
+
+  # ... and POST it back to GitHub
+  result = RestClient.post('https://github.com/login/oauth/access_token',
+                          {:client_id => CLIENT_ID,
+                           :client_secret => CLIENT_SECRET,
+                           :code => session_code},
+                           :accept => :json)
+
+  # extract the token and granted scopes
+  access_token = JSON.parse(result)['access_token']
+end
