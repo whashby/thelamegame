e5035651c84357dd69539e084b581b216a0500a0
whashby <whashby@gmail.com>
2025-05-01 19:12:52 -0400

    Update secrets.js

--- modules/secrets.js
+++ modules/secrets.js
@@ -1,4 +1,4 @@
-const token = ${{ GITHUB_TOKEN }};
-const token_type="Bearer";
+//const token = ${{ GITHUB_TOKEN }};
+//const token_type="Bearer";
 
-export {token,token_type};
+//export {token,token_type};
