fe29fdb5521ddd5a7198933b105591a69901eeb6
whashby <whashby@gmail.com>
2025-05-01 04:45:40 -0400

    Updated

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,18 @@
-"use strict";
-
 // src/index.js
 require('dotenv').config(); // Loads variables from .env into process.env
 
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-  res.send('Hello, Node.js with dotenv and a build script!');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    //res.send('Hello, Node.js with dotenv and a build script!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.listen(port, function () {
-  console.log("Server is running on port ".concat(port));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@
 require('dotenv').config(); // Loads variables from .env into process.env
 
 const express = require('express');
+const path = require('path');
 const app = express();
+
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.send('Hello, Node.js with dotenv and a build script!');
+    //res.send('Hello, Node.js with dotenv and a build script!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
